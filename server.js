a73fa3ee70905a68e287e1fe796f8ae2ae877105
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-02-05 14:56:52 -0800

    test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
 // const db = require('./app/models');
 
