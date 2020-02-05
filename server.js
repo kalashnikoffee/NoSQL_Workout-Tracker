c8c7fc5434cf541bc7a40cbe8f87d6745d0410c8
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-02-05 01:17:07 -0800

    more cleanup

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+const db = require('./app/models');
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", { useNewUrlParser: true });
+
+require("./app/routes/apiRoutes.js")(app);
+require("./app/routes/htmlRoutes.js")(app);
+
+//Uncomment Line 21 to create demo data
+// require('./seeders/seed.js')(db);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
+
 // //dependencies 
 // const express = require("express");
 // const mongoose = require("mongoose");
@@ -31,29 +57,3 @@
 // app.listen(PORT, () => {
 //     console.log(`App running on port ${PORT}!`);
 //   });
-
-const express = require("express");
-const mongoose = require("mongoose");
-const logger = require("morgan");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
-
-const db = require('./app/models');
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", { useNewUrlParser: true });
-
-require("./app/routes/apiRoutes.js")(app);
-require("./app/routes/htmlRoutes.js")(app);
-
-//Uncomment Line 21 to create demo data
-// require('./seeders/seed.js')(db);
-
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-});
